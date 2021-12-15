1fe152b0f9ea5b1ef0778fea3786352a663e529d
Indra Kusumah-kasim <shadeditor@gmail.com>
2021-12-15 11:34:53 +1100

    Basic python interface working with some testing - again

--- pythonInterface/core.py
+++ pythonInterface/core.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
+# Import libraries
 from typing import AnyStr
 import requests as rq
 
-class postHandler:
+# Define response codes
+codes = {
+    404:"Not Found",
+}
+
+class PostHandler:
     def __init__(self,url: AnyStr) -> None:
         self.url = url
 
-    def post(self,key,value):
-        response = rq.post(f'https://{self.url}/{key}',data={"id":value})
+    def post(self,key:AnyStr,value) -> bool:
+        # Creates post requests
+        try:
+            response = rq.post(f'http://{self.url}/post/{key}',json={"value":value},timeout=0.1)
+
+        # Checks for timeout
+        except rq.exceptions.Timeout:
+            print("HTTP POST ERROR: Request Timeout")
+            return False
+
+        # Prints the other kinds of errors just in case
+        except Exception as e:
+            print(f'HTTP POST ERROR: {e}')
+            return False
+
+        if response.status_code == 200:
+            return True
+        elif response.status_code in codes.keys():
+            print(f"HTTP POST ERROR: {response.status_code} {codes[response.status_code]}")
+            return False
+        else:
+            print(f"HTTP POST ERROR: Status Code {response.status_code}")
+            return False
