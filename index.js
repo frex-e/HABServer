ae2ff5d4b1cffcad0577615d69c2bb60fd1c2839
Indra Kusumah-kasim <shadeditor@gmail.com>
2021-12-16 13:04:33 +1100

    Make stuff look nicer

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const startTime = Date.now()
 
 // Defining todays date for log file
-let current_date =new Date();
+let current_date = new Date();
 const logfile = `logs/${current_date.getFullYear()}-${current_date.getMonth()+1}-${current_date.getDate()}_${current_date.getHours()}:${current_date.getMinutes()}:${current_date.getSeconds()}.log`;
 
 let current = {};
@@ -17,7 +17,7 @@ let log = [];
 app.use(express.json())
 
 // Displays stuff nicely
-function display(func){
+function display(func) {
     console.clear()
     console.log(`Alive at port: ${PORT}`)
     console.log(`Logging to file ${logfile}`)
@@ -26,27 +26,33 @@ function display(func){
 
 
 // Begins express server
-app.listen(PORT,()=>{
-    display(()=>console.log(""))
+app.listen(PORT, () => {
+    display(() => console.log(""))
 })
 
 
 
 //Listents at /post/
-app.post('/post/:id',(req,res)=>{
-    const { id } = req.params;
-    const { value } = req.body;
+app.post('/post/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    const {
+        value
+    } = req.body;
 
     // Checks for value, if empty, rejects
     if (!value) {
-        res.status(418).send({message:"No value"})
+        res.status(418).send({
+            message: "No value"
+        })
 
-    // Adds value to table and stores locally
+        // Adds value to table and stores locally
     } else {
         res.status(200).send();
-        log.push([(Date.now()-startTime)/1000,id,value])
+        log.push([(Date.now() - startTime) / 1000, id, value])
         current[id] = value;
-        display(()=>{
+        display(() => {
             console.table(current)
         })
         logData()
@@ -59,32 +65,34 @@ function logData() {
     let keys = Object.keys(current);
     let buffer = "time,";
     // Adds headers to the csv file.
-    for (var i = 0; i < keys.length; i++){
+    for (var i = 0; i < keys.length; i++) {
         buffer = buffer + keys[i] + ","
     };
 
     //Writes buffer to file
-    fs.writeFile(logfile,buffer,(err)=>{
+    fs.writeFile(logfile, buffer, (err) => {
         if (err) throw err;
         for (const entry of log) {
-            buffer = `\n${entry[0]}` + repeatStr(",", findIndexEqual(keys,entry[1])+1) +`${entry[2]}` + repeatStr(",", keys.length - findIndexEqual(keys, entry[1]));
+            buffer = `\n${entry[0]}` + repeatStr(",", findIndexEqual(keys, entry[1]) + 1) + `${entry[2]}` + repeatStr(",", keys.length - findIndexEqual(keys, entry[1]));
             fs.appendFile(logfile, buffer, () => {
                 if (err) throw err;
-             })
+            })
         }
 
-        });
+    });
 }
 
-function findIndexEqual(list,y){
-    return list.findIndex((x)=>{if (x==y) return true})
+function findIndexEqual(list, y) {
+    return list.findIndex((x) => {
+        if (x == y) return true
+    })
 }
 
 // Quick little function to repeat strings
-function repeatStr(str,num){
+function repeatStr(str, num) {
     let holder = ""
 
-    for (var i = 0; i<num;i++){
+    for (var i = 0; i < num; i++) {
         holder = holder + str
 
     }
